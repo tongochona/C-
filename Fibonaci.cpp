37d273ef334a7c3c58beb3e397127f50564fc3e0
tongochona <ngochoan15032003@gmail.com>
2023-01-28 10:20:12 +0700

    Cập nhật file Fibonaci.cpp

--- Fibonaci.cpp
+++ Fibonaci.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+// in ra số thứ n trong dãy fibonaci với f1=f2=1.
 int main(){
   int n, fn, fn1, fn2;
   cout <<"so thu ";
   cin>>n;
   fn=fn1=fn2=1;
   if (n==1||n==2) cout <<fn;
-  for(int i=3 ;i<=n; i++){
-  fn=fn1+fn2;
-  fn2=fn1;
-  fn1=fn;
+  else{
+    for(int i=3 ;i<=n; i++){
+    fn=fn1+fn2;
+    fn2=fn1;
+    fn1=fn;
   }
   cout <<fn;
+  }
   return 0;
 }
