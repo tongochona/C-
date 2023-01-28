cdbcbd0e29173224f74e1c0ab614b7fe6ae6cd02
tongochona <ngochoan15032003@gmail.com>
2023-01-28 09:48:46 +0700

    Cập nhật file CuocDienThoai.cpp

--- CuocDienThoai.cpp
+++ CuocDienThoai.cpp
@@ -7,7 +7,12 @@ int main(){
   cout <<"so phut goi la: ";
   cin >>a;
   if(a<=50) cout <<"so tien la: "<<25000+600*a;
-  if(a>50&&a<=200) cout <<"so tien la: "<<25000+600*50+400*(a-50);
-  if (a>200)cout <<"so tien la: "<<25000+600*50+400*150+200*(a-200);
+  else
+  {
+    if(a<=200) cout <<"so tien la: "<<25000+600*50+400*(a-50);
+    else{
+      cout <<"so tien la: "<<25000+600*50+400*150+200*(a-200);
+    }
+  }
   return 0 ;
 }
