a423af93b4e3423516ec7a8b8a07a647a65c9e13
tongochona <ngochoan15032003@gmail.com>
2023-01-30 20:45:23 +0700

    Hoàn thiện

--- MangSoAmLonNhat.cpp
+++ MangSoAmLonNhat.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
+// nhập vào dãy số, xuất ra số âm lớn nhất có trong mảng
 int main(){
   int n, max;
   cout<<"So phan tu trong mang: ";
   cin >>n;
   int x[n];
-  for (int i=1; i<=n;i++){
-    cout<<"Nhap so thu "<<i<<": ";
+  for (int i=0; i<n;i++){
+    cout<<"Nhap so thu "<<i+1<<": ";
     cin>>x[i];
   }
-  max =x[n];
-  for( int i=1; i<=n; i++){
+  max =-99999999;
+  for( int i=0; i<n; i++){
     if(x[i]<0) max=max>x[i]?max:x[i];
   }
   cout<<"So lon nhat trong mang la: " <<max;
