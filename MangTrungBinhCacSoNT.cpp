b72469071606bb25d13d229ff5030c95c2302a79
tongochona <ngochoan15032003@gmail.com>
2023-01-30 21:39:59 +0700

    hoàn thiện

--- MangTrungBinhCacSoNT.cpp
+++ MangTrungBinhCacSoNT.cpp
@@ -1,20 +1,30 @@
+//Nhập vào dãy số, in ra trung bình cộng các số nguyên tố có trong mảng
 #include <iostream>
 using namespace std;
-int mani(){
-  int n, m, tb, tong, a, j;
+// hàm kiểm tra số nguyên tố
+bool soNguyenTo(int k){
+  for(int i=2; i<=k/2; i++){
+    if(k%i==0) return false;
+  }
+  return true;
+}
+int main(){
+  int n, m=0, tong=0;
+  float tb;
+  cout<<"Nhap so phan tu";
   cin>> n;
   int x[n];
-  for (int i=1; i<=n;i++){
+  cout<<"nhap tung phan tu";
+  for (int i=0; i<n;i++){
     cin>>x[i];
-    for ( j=2; j<x[i];j++){
-      a=x[i]%j;
-      if (a==0) break;
-      }
-      if(j==x[i]){
-        tong+=x[i];
-        m++;
-      }
     }
+  for (int i=0; i<n; i++){
+    if(soNguyenTo(x[i])){
+      m++;
+      tong+=x[i];
+
+    } 
+  }  
     tb=tong/float(m);
     if (tb==0) cout<<"not found";
     else cout<<tb;
