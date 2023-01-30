d315f7bcb19584a5c6dafb1ef775f9bbdbc7660d
tongochona <ngochoan15032003@gmail.com>
2023-01-30 20:59:18 +0700

    hoàn thiện

--- MangSoChinhPhuong.cpp
+++ MangSoChinhPhuong.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
+//nhập vào dãy số, xuất ra số chính phương
 int main(){
   int n, a;
   cin >>n;
   int x[n];
-  for (int i=1; i<=n;i++){
+  for (int i=0; i<n;i++){
     cin>> x[i];
   }
-  for(int i=1;i<=n;i++){
-    for(int j=1; j<x[i];j++){
+  for(int i=0;i<n;i++){
+    for(int j=1; j<=sqrt(x[i]);j++){
       a=j*j;
       if (a==x[i]) cout << x[i]<<" ";
     }
