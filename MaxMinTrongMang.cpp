aa352147b5bacdaa8546e495de531da7df229dc5
tongochona <ngochoan15032003@gmail.com>
2023-01-31 08:46:50 +0700

    hoàn thiện

--- MaxMinTrongMang.cpp
+++ MaxMinTrongMang.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
+//Nhập vào mảng số nguyên, in ra số coá giá trị lớn nhất và nhỏ nhất
 int main(){
   int a, max, min;
   cin >>a;
   int x[a];
-  for (int i=1; i<=a; i++){
+  for (int i=0; i<a; i++){
     cin >>x[i];
-    if (i==1) max =x[1], min=x[1];
+  }
+  max =x[0], min=x[0];
+  for(int i=0; i<a; i++){
     max = max >x[i]?max:x[i];
     min = min<x[i]?min:x[i];
   }
