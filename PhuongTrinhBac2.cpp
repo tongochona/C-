fdbd3d5ebf293ec59a363221ffe3c1c5230ed353
tongochona <ngochoan15032003@gmail.com>
2023-01-31 09:15:12 +0700

    hoàn thiện

--- PhuongTrinhBac2.cpp
+++ PhuongTrinhBac2.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-
+//Giải phương trình bậc 2
 int main(){
 float a, b, c, delta;
 cout <<"nhap a, b, c: ";
 cin>>a>>b>>c;
 delta=pow(b,2)-4*a*c;
 if (a==0) {
-  if (b==c&&b==0) cout<<"VSN";
-  if (b==0&&c!=0) cout <<"VN" ;
-  if(b!=0) cout<<"x= "<<-c/b<<endl;
+  if (b==0){
+    if(c==0) cout<<"Phuong trinh vo so nghiem!";
+    else cout<<"Phuong trinh vo nghiem";
+    
+  }
+  else cout<<"x= "<<-c/b<<endl;
 }
 else {
   if (delta<0) cout <<"VN";
-  if (delta==0) cout <<"x= "<<-b/(2*a);
-  if (delta>0) cout<<"x1= "<<(-b+sqrt(delta))/(2*a) <<"  " <<"x2= "<<(-b-sqrt(delta))/(2*a);
+  else {
+    if(delta==0) cout <<"x= "<<-b/(2*a);
+    else cout<<"x1= "<<(-b+sqrt(delta))/(2*a) <<"  " <<"x2= "<<(-b-sqrt(delta))/(2*a);
+  }
 }  return 0;
 }
