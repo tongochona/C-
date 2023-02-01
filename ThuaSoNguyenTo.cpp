a8f015ac1bd1ff2ba88fd4325892801fb5b15054
tongochona <ngochoan15032003@gmail.com>
2023-02-01 22:22:30 +0700

    tối ưu, giảm được ít phép so sánh

--- ThuaSoNguyenTo.cpp
+++ ThuaSoNguyenTo.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+//nhập vào một số, in ra tích của các thừa số nguyên tố tạo nên nó. Ví dụ 6=2*3.
 int main(){
   int a, b, i, x;
   cin>>a;
   i=2;
-  while (a!=0){
+  while (a!=1){
     x=a%i;
     if (x==0){
       cout<<i;
-      if(i>a/2) break;
+      //if(i>a/2) break;
       cout<<"*";
+      a/=i;
     }
     else i++;
-    if (x==0) a/=i;
   }
   return 0 ;
 }
